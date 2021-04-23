cfea871d6c2a56529f8f11ab00315993282e2f13
unknown <diviyani.vel@gmail.com>
2021-04-23 09:40:32 +0530

    first commit

---
+++ day2 proj1.cs
@@ -0,0 +1,15 @@
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[][] jagged_arr= new int[][]
+        {
+            new int[]{1,2,3};
+            new int[]{11,23};
+        }
+        
+    }
+}
---
+++ dynamic string.cs
@@ -0,0 +1,13 @@
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        dynamic i= "diviyani";
+        i=1;
+        Console.WriteLine(i);
+        Console.ReadLine();
+    }
+}
