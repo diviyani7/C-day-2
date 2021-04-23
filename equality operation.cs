f62a2ed2077398f6a57410823755e596fae83d0d
unknown <diviyani.vel@gmail.com>
2021-04-23 16:26:17 +0530

    first commit

---
+++ equality operation.cs
@@ -0,0 +1,15 @@
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        object str = "hello" ;
+        char[] values = {'h' ,'e', 'l','l','o'};
+        object str2 = new string(values);
+        Console.WriteLine("Using Equality operator " +(str== str2));
+        Console.WriteLine("Using equals() method {0}", str.Equals(str2));
+        Console.ReadLine();
+    }
+}
---
+++ name string.cs
@@ -0,0 +1,17 @@
+using System;
+namespace Day
+{
+    class Class1
+    {
+        public static void addstring(string s1,string s2,string s3)
+        {
+            Console.WriteLine(s1+s2+s3);
+            
+        }
+        static void Main()
+        {
+            addstring(s1:"Diviyani",s2:" ",s3:"Coumaravel");
+            Console.ReadLine();
+        }
+    }
+}
